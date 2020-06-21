1cb436e90d791357b310415b87b575350552098d
Alejandro Villafañe <info@alezvi.com.ar>
2020-06-21 15:31:34 -0300

    Renamed dirs

---
+++ exercises/ciclos/loops-1.js
@@ -0,0 +1,22 @@
+/*
+
+
+Analizando el siguiente bloque de código
+
+```
+	variable inicio = 1
+	inicio es menor a 5
+	inicio++
+
+	comienzo
+		imprimir "Hola"
+	fin
+```
+¿Cuántas veces se imprimirá la palabra "Hola"?
+
+*/
+
+module.exports = {
+    result: 5
+}
+
---
+++ exercises/logica/logic-1.js
@@ -0,0 +1,20 @@
+/*
+
+La lógica es el punto de partida para que un programa funcione, ya que nos permitirá que ocurra una cosa u otra de forma dinámica. 
+
+Vamos a comenzar una verificación básica. ¿Qué debería imprimir el siguiente código?
+
+```
+variable estoyAprendiendo = verdadero
+si (estoyAprendiendo)
+	imprimir "Estoy aprendiendo"
+si no
+	imprimir "Momento de descanso"
+fin
+```
+
+*/
+
+module.exports = {
+    result: 'Estoy aprendiendo'
+}
