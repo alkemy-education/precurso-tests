874855e3c8b02b81bf6d4989ba8d7681732fe947
Alejandro Villafañe <info@alezvi.com.ar>
2020-06-24 11:30:13 -0300

    Testing example

--- test/variables/1.js
+++ test/variables/1.js
@@ -1,18 +1,26 @@
 const expect = require("chai").expect;
 const path = require('path')
-const fileName = path.resolve(__dirname, '..', '..', 'exercises', 'variables', '1.js')
-const exercise = require(fileName)
+const fs = require('fs')
+const fileName = path.resolve(__dirname, '..', '..', 'src', 'variables', '1.js')
+const exercise = fs.readFileSync(fileName, {encoding: "utf-8"})
+const tester = require('../helpers/parser')
+const varTester = require('../helpers/variables')
+
+let script = tester(exercise)
+let variableNombre = script.declaresVariable('nombre')
+let variableApellido = script.declaresVariable('apellido')
+let variableEdad = script.declaresVariable('edad')
 
 describe("Deberás declarar tres variables llamadas nombre, apellido y edad", () => {
     it("La variable nombre debe estar declarada", () => {
-        expect(exercise.nombre).to.not.be.undefined
-    });
+        expect(variableNombre).to.not.be.undefined
+    })
 
     it("La variable apellido debe estar declarada", () => {
-        expect(exercise.apellido).to.not.be.undefined
-    });
+        expect(variableApellido).to.not.be.undefined
+    })
 
     it("La variable edad debe estar declarada", () => {
-        expect(exercise.edad).to.not.be.undefined
-    });
-});
+        expect(variableEdad).to.not.be.undefined
+    })
+})
